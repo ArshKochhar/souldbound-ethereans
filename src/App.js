f6607cdb6565deb5b78e44dd7b4547c378043247
Arsh Kochhar <arshkochhar15@gmail.com>
2022-08-02 00:01:59 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import logo from "./assets/Ethereum-Logo-PNG.png";
+import "./App.css";
+import { useAnimate } from "react-simple-animate";
+import { useEffect } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { style, play } = useAnimate({ start: { opacity: 0 }, end: { opacity: 1 } });
+    useEffect(() => play(true), []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Container>
+                    <p className="mb-4" style={{ fontWeight: 100, fontSize: "80px", paddingBottom: "10px" }}>
+                        Welcome Etherean!{" "}
+                        <span className="mb-4">
+                            <Button variant="outline-light" className="mb-2" style={{ fontWeight: 90, fontSize: "4vw", paddingLeft: "20px", paddingRight: "20px", borderRadius: "30px" }}>
+                                {" "}
+                                Connect your wallet
+                            </Button>
+                        </span>
+                    </p>
+                    <div>
+                        <img style={style} src={logo} className="App-logo" alt="logo" />
+                    </div>
+                </Container>
+            </header>
+        </div>
+    );
 }
 
 export default App;
